8ca5635275dc866d563bf2d828fdecffbb0717d1
PaulGlasemann <PaulGlasemann@0-208-11>
2020-09-18 12:54:02 +0200

    los

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,16 +3,17 @@ public class HelloWorld {
 
 	static int counter = 0;
 	
+	/* das war ein Programm aus der Schule, es zählt und so */
 	public static void main(String[] args)  {
 	
 		while(counter<100) {
 		if(counter%5==0)
 		{System.out.println("HA");}
-	/*	else { System.out.println("HelloWorld");}
+		else { System.out.println("HelloWorld");}
 		
 		System.out.println(counter);
 		
-		counter++;   */
+		counter++;   
 	
 		}
 	}
