1c740bb0f31bf37da3488d90b80c49c39c2e33df
PaulGlasemann <PaulGlasemann@0-208-11>
2020-09-18 12:52:31 +0200

    ist besser so

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,11 +1,19 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) 
+	static int counter = 0;
 	
-	                            {
-	 System.out.println("HelloWorld");
-
+	public static void main(String[] args)  {
+	
+		while(counter<100) {
+		if(counter%5==0)
+		{System.out.println("HA");}
+	/*	else { System.out.println("HelloWorld");}
+		
+		System.out.println(counter);
+		
+		counter++;   */
+	
+		}
 	}
-
 }
