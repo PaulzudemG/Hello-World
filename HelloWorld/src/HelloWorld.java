6123ba554888b8a56b1770509e9fc1ffcffee51c
PaulGlasemann <PaulGlasemann@0-208-11>
2020-09-25 11:59:52 +0200

    fissbuss

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,17 +1,20 @@
 
 public class HelloWorld {
 
-	static int counter = 0;
+	static int counter = 1;
 	
 	/* das war ein Programm aus der Schule, es zählt und so */
 	public static void main(String[] args)  {
 	
-		while(counter<100) {
-		if(counter%5==0)
-		{System.out.println("HA");}
-		else { System.out.println("HelloWorld");}
+		while(counter<=100) {
+			if(counter%3==0 && counter%5==0) {
+				System.out.println("Fissbuss");
+			}
+		   if(counter%3==0)  {System.out.println("Fiss");}
+		      else 
+		         if (counter%5==0) {System.out.println("Buss");}
+		           else {System.out.println(counter);}
 		
-		System.out.println(counter);
 		
 		counter++;   
 	
